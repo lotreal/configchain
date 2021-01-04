a0a151c597d9d23642b43ed8a403bdb8da965772
Luo Tao <lotreal@gmail.com>
2021-01-04 11:35:29 +0800

    chore: import bumpversion

--- configchain/__init__.py
+++ configchain/__init__.py
@@ -6,6 +6,8 @@ from .config import Config
 from .configset import ConfigSet
 from .types import ConfigFile, ConfigChainOptions
 
+__version__ = "0.3.8"
+
 
 def configchain(*files: ConfigFile, **kwargs: ConfigChainOptions) -> ConfigSet:
     return reduce(add, [ConfigSet.load(f, **kwargs) for f in files])
